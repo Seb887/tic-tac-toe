d8b25be496df74796966aa1361a7422c6c7b195c
Seb887 <seb.dev@posteo.de>
2023-04-10 13:52:56 +0200

    class Gameboard

--- app.js
+++ app.js
@@ -1,1 +1,52 @@
 'use strict';
+
+class Gameboard {
+  constructor(playerX, playerO) {
+    this.playerSignX = playerX;
+    this.playerSignO = playerO;
+    this.gameboardArr = ['', '', '', '', '', '', '', '', ''];
+    this.html = this.createHTML;
+  }
+  createHTML() {
+    const gameboardContainer = document.querySelector('.gameboardContainer');
+
+    let gameboard = document.createElement('div');
+    gameboard.classList.add(
+      'gameboard',
+      'grid',
+      'justify-center',
+      'grid-cols-3',
+      'relative',
+      'top-72',
+      'w-96',
+      'h-96',
+      'border',
+      'border-sky-500'
+    );
+
+    for (let i = 0; i < this.gameboardArr.length; i++) {
+      const field = document.createElement('div');
+      field.classList.add(
+        'field',
+        'flex',
+        'justify-center',
+        'items-center',
+        'text-5xl',
+        'font-extrabold',
+        'text-slate-400',
+        'border',
+        'border-sky-500'
+      );
+      field.setAttribute('id', `field-${i + 1}`);
+
+      gameboard.appendChild(field);
+    }
+
+    gameboardContainer.appendChild(gameboard);
+  }
+
+  checkWinner() {}
+}
+
+const newGameboard = new Gameboard();
+newGameboard.createHTML();
